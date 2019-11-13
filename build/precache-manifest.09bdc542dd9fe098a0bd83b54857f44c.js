830aaf73d7d87061e3c715e0dd0012f6c77a0a56
Ricardo <568082704@qq.com>
2019-11-13 15:23:34 +0800

    save

---
+++ build/precache-manifest.09bdc542dd9fe098a0bd83b54857f44c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41457f23d5023e0e468f337ab0d847f",
+    "url": "/redux_todoList/build/index.html"
+  },
+  {
+    "revision": "ac0b2e82e35c41c6375e",
+    "url": "/redux_todoList/build/static/css/main.fc49ad4f.chunk.css"
+  },
+  {
+    "revision": "de8d0324c0efd426b81a",
+    "url": "/redux_todoList/build/static/js/2.c67db90a.chunk.js"
+  },
+  {
+    "revision": "ac0b2e82e35c41c6375e",
+    "url": "/redux_todoList/build/static/js/main.50279823.chunk.js"
+  },
+  {
+    "revision": "df75529ffb0700f5a0b2",
+    "url": "/redux_todoList/build/static/js/runtime-main.59a7b8b4.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37204062f161e89f6e33247f08c095fa.js"
+  "/redux_todoList/build/precache-manifest.09bdc542dd9fe098a0bd83b54857f44c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux_todoList/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
